d6a1b7115a6ac00501332c325db3163e1a7d2655
spypire <spypire22092547@gmail.com>
2021-04-16 15:02:10 +0700

    Exercise4_Spypire_P.py

---
+++ Exercise4_Spypire_P.py
@@ -0,0 +1,10 @@
+FE_Eng = 14
+GB = 12.5
+ICS = 18
+CP = 11
+
+print("--------your score----------")
+print("Founding English                  ","=",FE_Eng)
+print("General Bussiness                 ","=",GB)
+print("Introduction to Computer Systems  ","=",ICS)
+print("computer Programming              ","=",CP)
