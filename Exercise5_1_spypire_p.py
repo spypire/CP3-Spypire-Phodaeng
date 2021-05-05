d8a840e33679a4d6661839c0075f2e2c9beab6fe
spypire <spypire22092547@gmail.com>
2021-05-05 22:55:48 +0700

    Create Exercise5_1_spypire_p.py

---
+++ Exercise5_1_spypire_p.py
@@ -0,0 +1,12 @@
+print("------calculator------")
+x = int(input("First Number:"))
+y = int(input("Second Number:"))
+result1= x+y
+result2= x-y
+result3= x*y
+result4 = x/y
+
+print(x,"+",y,"=",result1)
+print(x,"-",y,"=",result2)
+print(x,"*",y,"=",result3)
+print(x,"/",y,"=",result4)
